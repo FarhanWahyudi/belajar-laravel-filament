bc12afbb27ec9b90f48a98f5481be008b4e2364c
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-19 09:13:30 +0700

    Notifications

--- app/Filament/Resources/BarangResource/Pages/CreateBarang.php
+++ app/Filament/Resources/BarangResource/Pages/CreateBarang.php
@@ -4,9 +4,21 @@ namespace App\Filament\Resources\BarangResource\Pages;
 
 use App\Filament\Resources\BarangResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBarang extends CreateRecord
 {
     protected static string $resource = BarangResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Berhasil dibuat')
+            ->iconColor('danger')
+            ->color('warning')
+            ->duration(2000)
+            ->body('Data Berhasil dibuat');
+    }
 }
