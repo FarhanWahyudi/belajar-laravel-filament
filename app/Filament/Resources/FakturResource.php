997694c05cb58e25f715aadef881deb0c184f8e6
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-19 07:37:30 +0700

    Field Lifecycle

--- app/Filament/Resources/FakturResource.php
+++ app/Filament/Resources/FakturResource.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\FakturResource\Pages;
 use App\Filament\Resources\FakturResource\RelationManagers;
+use App\Models\Barang;
+use App\Models\Customer;
 use App\Models\Faktur;
 use Filament\Forms;
 use Filament\Forms\Components\DatePicker;
@@ -11,6 +13,8 @@ use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -37,36 +41,83 @@ class FakturResource extends Resource
                         'sm' => 1,
                         'md' => 2
                     ]),
-                TextInput::make('kode_customer')
-                    ->columnSpan(1),
                 Select::make('customer_id')
-                    ->relationship('customer', 'nama_customer'),
-                TextInput::make('ket_faktur'),
-                TextInput::make('total'),
-                TextInput::make('nominal_charge'),
-                TextInput::make('charge'),
-                TextInput::make('total_final'),
+                    ->reactive()
+                    ->relationship('customer', 'nama_customer')
+                    ->afterStateUpdated(function ($state, callable $set) {
+                        $customer = Customer::find($state);
+
+                        if ($customer) {
+                            $set('kode_customer', $customer->kode_customer);
+                        }
+                    }),
+                TextInput::make('kode_customer')
+                    ->columnSpan(1)
+                    ->disabled()
+                    ->dehydrated(),
                 Repeater::make('details')
                     ->relationship()
                     ->schema([
                         Select::make('barang_id')
-                            ->relationship('barang', 'nama_barang'),
-                        TextInput::make('diskon')
+                            ->relationship('barang', 'nama_barang')
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                $barang = Barang::find($state);
+                                if($barang) {
+                                    $set('nama_barang', $barang->nama_barang);
+                                    $set('harga', $barang->harga_barang);
+                                }
+                            }),
+                            TextInput::make('nama_barang'),
+                            TextInput::make('harga')
                             ->numeric(),
-                        TextInput::make('nama_barang'),
-                        TextInput::make('harga')
+                            TextInput::make('qty')
+                            ->numeric()
+                            ->reactive()
+                            ->afterStateUpdated(function($state, Set $set, Get $get) {
+                                $harga = $get('harga');
+                                $set('hasil_qty', $harga * $state);
+                            }),
+                            TextInput::make('hasil_qty')
                             ->numeric(),
-                        TextInput::make('subtotal')
-                            ->numeric(),
-                        TextInput::make('qty')
-                            ->numeric(),
-                        TextInput::make('hasil_qty')
+                            TextInput::make('diskon')
+                            ->numeric()
+                            ->reactive()
+                            ->afterStateUpdated(function($state, Set $set, Get $get) {
+                                $hasilQty = $get('hasil_qty');
+                                $diskon = $hasilQty * ($state / 100);
+                                $total = $hasilQty - $diskon;
+                                $set('subtotal', intval($total));
+                            }),
+                            TextInput::make('subtotal')
                             ->numeric(),
                     ])->columnSpan([
                         'default' => 1,
                         'md' => 2,
                         'lg' => 3
-                    ])
+                    ])->live(),
+                TextInput::make('ket_faktur'),
+                TextInput::make('total')
+                    ->placeholder(function (Set $set, Get $get) {
+                        $detail = collect($get('details'))->pluck('subtotal')->sum();
+                        if($detail == null) {
+                            $set('total', 0);
+                        } else {
+                            $set('total', $detail);
+                        }
+                    }),
+                TextInput::make('nominal_charge')
+                    ->reactive()
+                    ->afterStateUpdated(function ($state, Set $set, Get $get) {
+                        $total = $get('total');
+                        $charge = $total * ($state / 100);
+                        $hasil = $total + $charge;
+
+                        $set('total_final', $hasil);
+                        $set('charge', $charge);
+                    }),
+                TextInput::make('charge'),
+                TextInput::make('total_final'),
             ]);
     }
 
